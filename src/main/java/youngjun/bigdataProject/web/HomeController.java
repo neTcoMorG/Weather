1b04f1a3e8b2fba479584345b6b5b20fac04186b
joyoungjun <joyoungjun8590@gmail.com>
2023-02-28 03:53:05 +0900

    UI 수정

--- src/main/java/youngjun/bigdataProject/web/HomeController.java
+++ src/main/java/youngjun/bigdataProject/web/HomeController.java
@@ -1,16 +1,19 @@
 package youngjun.bigdataProject.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 @EnableJpaAuditing
 public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
+        log.info("접속감지");
         model.addAttribute("test", "hello, world");
         return "korea";
     }
