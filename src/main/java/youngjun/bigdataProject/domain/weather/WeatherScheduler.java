fee758cd6d18e44d40f97f7940a4100fd9912ad5
joyoungjun <joyoungjun8590@gmail.com>
2023-02-28 17:49:05 +0900

    불필요한 코드 정리

--- src/main/java/youngjun/bigdataProject/domain/weather/WeatherScheduler.java
+++ src/main/java/youngjun/bigdataProject/domain/weather/WeatherScheduler.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import youngjun.bigdataProject.domain.entity.Region;
 import youngjun.bigdataProject.domain.entity.Weather;
 import youngjun.bigdataProject.domain.repository.MemoryCacheRepository;
@@ -13,6 +12,8 @@ import youngjun.bigdataProject.domain.weather.api.mapping.WeatherData;
 import youngjun.bigdataProject.domain.repository.RegionRepository;
 import youngjun.bigdataProject.domain.repository.WeatherRepository;
 import youngjun.bigdataProject.web.Init;
+
+import javax.transaction.Transactional;
 
 @Slf4j
 @Component
@@ -23,13 +24,12 @@ public class WeatherScheduler {
     private final RegionRepository regionRepository;
     private final MemoryCacheRepository memoryCacheRepository;
 
-    @Transactional
-    public void core() throws Exception {
-//        for (String region : Init.REGIONS) {
-//            WeatherData data = Api.getWeather(region);
-//            weatherRepository.save(createWeatherEntity(region, data));
-//        }
+    @Scheduled(cron = "0 0 0/1 * * *")
+    public void processor() throws Exception {
+        collect();
+    }
 
+    private void collect () throws Exception {
         for (int i=0; i<Init.REGIONS.length; i++) {
             WeatherData data = Api.getWeather(Init.REGIONS[i]);
             weatherRepository.save(createWeatherEntity(Init.KOREAN[i], data));
@@ -40,10 +40,5 @@ public class WeatherScheduler {
     private Weather createWeatherEntity (String region, WeatherData data) {
         Region findRegion = regionRepository.findByName(region).orElseThrow();
         return new Weather(findRegion, data);
-    }
-
-    @Scheduled(cron = "0 0 0/1 * * *")
-    public void processor() throws Exception {
-        core();
     }
 }
